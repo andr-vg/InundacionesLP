51e9153fde2857aae461b1d172d8c41aa91f04ae
andreagoyechea <andreagoyechea@gmail.com>
2021-10-12 23:05:39 -0300

    flash a form

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -10,16 +10,14 @@ def login():
 
 def authenticate():
     params=request.form
-
+    flash(params.email)
     user=(User.query.filter(and_(User.deleted==False,User.active==True)) 
     .filter(and_(User.email == params["email"],User.password == params["password"])).first()
     )
     print(user)
     if not user:
-        flash(user.email)
         flash("Usuario o clave incorrecto.")
         return redirect(url_for("auth_login"))
-    flash(user.email)
     session["user"] = user.email
     # save configuration params 
     session["config"] = Configuration.query.filter().first()

237822d92f815053e753a5fa2311eaae1677fcae
diegoag30 <diego.pablo.aguilar@gmail.com>
2021-11-02 01:37:56 -0300

    Archivo subido ahora es procesado como csv

---
+++ app/helpers/csv_check.py
@@ -0,0 +1,8 @@
+def check(filename):
+    """
+    Verifica que el archivo recibido tenga extension .csv
+    Args:
+        filename: Nombre del archivo a verificar
+    """
+    return '.' in filename and \
+    filename.rsplit('.', 1)[1].lower() == "csv"
--- app/resources/zonas_inundables.py
+++ app/resources/zonas_inundables.py
@@ -2,35 +2,31 @@ import csv
 from pathlib import Path
 from flask import redirect, render_template, request, url_for, session, abort, flash
 import json
-
+from app.helpers.csv_check import check
 from flask.templating import render_template
+import io
 
 def index(page):
     return render_template("zonas_inundables/index.html",page=1)
 
 def upload():
     file = request.files["inputFile"]
+    file_content = io.TextIOWrapper(file.stream._file, "UTF8", newline=None)
+    print(check(file.filename))
+    if check(file.filename):
+        _process_csv(file_content)
     return file.filename
-"""
-#Path de zonas.csv
-path = Path.cwd() 
-zonas_folder = path.parent /'grupo22' /'app' / 'data'
-file = zonas_folder / 'zonas.csv'
-json_file_path = zonas_folder / 'zonas.json'
 
-#Lectura de zonas.csv
-values = {}
-with open(file) as csv_file:
-    d_reader = csv.DictReader(csv_file)
+def _process_csv(file):
+    #Path de zonas.csv
+    zonas_folder = Path.cwd().parent /'grupo22' /'app' / 'data'
+    json_file_path = zonas_folder / 'zonas.json'
+    #Lectura de zonas.csv
+    values = {}
+    d_reader = csv.DictReader(file)
     for index,row in enumerate(d_reader):
         zones_list = json.loads(row['area'])
         values[index] = row
         values[index]["area"]= zones_list
-print(values[0]["area"][1])
-
-
-
-
-#with open(json_file_path,'w') as json_file:
-#    json_file.write(json.dumps(values, indent=4))
-"""
+    with open(json_file_path,'w') as json_file:
+        json_file.write(json.dumps(values, indent=4))

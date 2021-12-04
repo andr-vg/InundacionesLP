392f6accf2f80f22684b01d7a118a02e30a0803d
andreagoyechea <andreagoyechea@gmail.com>
2021-12-03 22:20:42 -0300

    home colores

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,28 +1,40 @@
 <template>
 
 <head_text > 
-   <h1>Quienes somos? </h1>
-        
-  <h5>Inundacion de La Plata 2013</h5> </head_text>
-  <p>Por medio de esta pagina podra saber aquellas zonas que estan en peligro de inundacion, y en el caso de que sucedan , que actividades se pueden realizar. 
-    Entre estas se encuentrarn los recorridos de evacuacion y puntos de encuentro cercanos a su localidad.
-    <br> Podra redactar una denuncia en caso de encontrar un lugar problematico
-      </p>
+  <h1>Quiénes somos</h1>
+  <big_img>
+    <p>Por medio de esta página podrá saber aquellas zonas que están en peligro de inundación, y qué acciones llevar a cabo según el área en el que se encuentre. Entre éstas se encuentran los recorridos de evacuación y puntos de encuentro cercanos a su localidad.
+    <br>
+    También podrá redactar una denuncia en caso de encontrar basurales o alcantarillas tapadas.
+  </p>
+  </big_img>
+
+  <h3>Inundación de La Plata 2013</h3>
+  <big_img>
+      <p>La inundación en La Plata de 2013 ocurrió el 2 de abril de 2013, con un registro de precipitaciones en la región con más de 400 mm acumulados en cuatro horas. Las ciudades de Ensenada y Berisso y los barrios platenses de Los Hornos, Villa Elvira y Tolosa fueron los más afectados junto al casco céntrico de la ciudad.</p>
+
+  <h4>Plan de Gestión Integrada de Riesgos de Desastres</h4>
+  <p>La Universidad Nacional de La Plata junto con la Universidad Tecnológica Nacional propusieron un Plan de Gestión Integrada de Riesgos de Desastres (GRID), y crearon la colección Emergencia Hídrica dentro del Servicio de Difusión de la Creación Intelectual, el repositorio institucional de la UNLP, para reunir trabajos (artículos, informes técnicos, libros y otros) que contribuyan al desarrollo del plan y sirvan como base de datos para futuros trabajos en el área.</p>
+  </big_img>
+  
+</head_text>
+
 <content>
+  
 	<br>
 	<line_cards>
 		<card_box>
 			<card><h3>Puntos de encuentro y recorridos de evacuación</h3>
        <img class="imagen_tarjeta" src="../assets/punto_recorrido.png" alt="mapa_punto_recorrido">
-       <p>Puntos de encuentro en caso de inundaciones y rutas para llegar a estos</p>
+       <p>Puntos de encuentro en caso de inundaciones y rutas para llegar a éstos.</p>
       </card>
       <card><h3>Zonas inundables</h3>
       <img class="imagen_tarjeta2" src="../assets/zona.png" alt="mapa_punto_recorrido">
-      <p> Informate sobre las zonas que estan en riesgo de inundacion </p>
+      <p> Informate sobre las zonas que están en riesgo de inundacion. </p>
       </card>
       <card><h3>Realizar denuncias</h3>
        <img class="imagen_tarjeta2" src="../assets/denuncia.png" alt="mapa_punto_recorrido">
-       <p> Preveni inundaciones al realizar denuncias sobre lugares peligrosos de tu ciudad</p>
+       <p> Prevení inundaciones al realizar denuncias sobre lugares peligrosos de tu ciudad.</p>
       </card>
 		</card_box>
 	</line_cards>

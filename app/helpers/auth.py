85bb8ddbb9d4a0cd158e997affa804f7e1fe5725
Zarate, Luciano Andrés <zaratelucianoa@gmail.com>
2021-12-16 14:22:30 -0300

    Cambios en helper is_pending()

--- app/helpers/auth.py
+++ app/helpers/auth.py
@@ -1,6 +1,3 @@
-from app.models.user import User
-
-
 def authenticated(session):
     """
     Retorna el email del usuario autenticado
@@ -12,5 +9,4 @@ def get_pending_state(session):
     """
     Retorna 1 si el usuario esta pendiente, 0 si no lo esta
     """
-    user = User.get_user_by_email(session.get("user"))
-    return user.pending
+    return session.get("pending")
--- app/resources/auth.py
+++ app/resources/auth.py
@@ -96,6 +96,8 @@ def callback(google_client_id, google_client_secret, google_discovery_url):
             session["config"] = Configuration.get_configuration()
             session["permissions"] = User.get_permissions(user_id=user.id)
             session["pending"] = user.pending
+        if user.pending:
+            session["pending"] = user.pending
     return redirect(url_for("home"))
 
 
